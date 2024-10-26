4e47cf5fa9b93bd1a573aecb9d6427aff1e1fd01
PESolut <jahaadpetty@pursuit.org>
2024-10-26 09:51:29 -0400

    instructions added

---
+++ javascript/DS/static-arrays/StaticArray.js
@@ -0,0 +1,21 @@
+/*
+Task: Implement a static array class in JavaScript.
+
+core methods:
+constructor
+destructor
+
+basic operations:
+get
+set
+size
+
+utility methods:
+insert
+delete
+find
+clear
+isEmpty
+isFull
+*/
+
