a1906b53694f64eab810b7df8252e14f8be80fbf
PESolut <jahaadpetty@pursuit.org>
2024-10-26 15:15:50 -0400

    base question added

---
+++ challenges/javascript/26-remove-duplicates-from-sorted-array/removeDuplicates.js
@@ -0,0 +1,15 @@
+/**
+ * Removes duplicates from a sorted array in-place.
+ * @param {number[]} nums - An array of numbers sorted in non-decreasing order.
+ * @returns {number} The number of unique elements in the array.
+ * @constraints
+ * - 0 <= nums.length <= 3 * 10^4
+ * - -100 <= nums[i] <= 100
+ * - nums is sorted in non-decreasing order
+ */
+const removeDuplicates = (nums) => {
+
+}
+
+console.log(removeDuplicates([1, 1, 2])) // => [1, 2]
+console.log(removeDuplicates([0, 0, 2, 2, 3])) // => [0, 2, 3]
